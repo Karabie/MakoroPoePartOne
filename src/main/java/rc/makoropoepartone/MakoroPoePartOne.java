6e7e3c541ee16ebdb849883c917ef2aa3b0b0ff0
@KaraboMakoro <k.m.makoro@gmail.com>
2024-04-22 19:30:08 +0200

    Initial commit

---
+++ src/main/java/rc/makoropoepartone/MakoroPoePartOne.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package rc.makoropoepartone;
+
+/**
+ *
+ * @author kmmak
+ */
+public class MakoroPoePartOne {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
